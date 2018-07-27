1eea7f549cc7aef07c55608de80b798e15d6c53f
Zachary Itskin <36995254+Zayitskin@users.noreply.github.com>
2018-07-27 14:28:08 -0500

    Add files via upload

---
+++ ringDelay.py
@@ -0,0 +1,23 @@
+import mido, time
+
+inport = mido.open_input("SoTASPort", virtual=True)
+outport = mido.open_output("TiMidity port 0")
+
+buffer = []
+
+try:
+    while True:
+        clock = time.time()
+        
+        for msg in inport.iter_pending():
+            buffer.append([msg, clock + 1])
+
+        for msg in buffer:
+            print(msg)
+            if clock >= msg[1]:
+                outport.send(msg[0])
+                buffer.remove(msg)
+
+except KeyboardInterrupt:
+    inport.close()
+    outport.close()
